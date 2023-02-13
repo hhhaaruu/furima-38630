d9f6d2400c8cdc9f58fdf4d8a7e1918b7a7fc93d
hhhaaruu <118385602+hhhaaruu@users.noreply.github.com>
2023-02-14 06:11:02 +0900

    バリデーション設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :user_parameters, if: :devise_controller?
 
   private
+  def user_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_gate ])
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
