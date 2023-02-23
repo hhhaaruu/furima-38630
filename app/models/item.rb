fe49876f5f0d1ff553bb673f39cadf91c21e2d6e
hhhaaruu <118385602+hhhaaruu@users.noreply.github.com>
2023-02-23 17:58:26 +0900

    カテゴリー表示の追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,30 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+  belongs_to :shipping_fee_status
+  belongs_to :sales_status
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :item_name
+    validates :item_info
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+    validates :item_price
+  end
+
+  with_options numericality: { other_than: 0 , message: "Can't be blanck"} do
+    validates :category_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+    validates :shipping_fee_status_id
+    validates :sales_status_id
+  end
+
 end
