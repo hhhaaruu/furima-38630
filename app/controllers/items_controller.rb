ac899abf936288358d010e4c556da3802252f7ec
hhhaaruu <118385602+hhhaaruu@users.noreply.github.com>
2023-03-01 01:01:18 +0900

    商品出品機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,11 @@ class ItemsController < ApplicationController
   end
 
   def new
+   if user_signed_in? 
     @item = Item.new
+   elsif
+    redirect_to new_user_session_path
+   end
   end
 
   def create
--- app/models/item.rb
+++ app/models/item.rb
@@ -12,8 +12,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :item_info
-    validates :item_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-    format: { with: /\A[0-9]+\z/ }
+    validates :item_price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
 
     validates :image
   end
